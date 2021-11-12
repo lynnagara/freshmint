45354391764c8de7b0c78ad001a0f39466d258b5
Lyn <lyn.nagara@gmail.com>
2021-11-11 23:18:47 -0800

    add integration test

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
 serve:
 	cd server && cargo run
-
 .PHONY: serve
+
+test-server:
+	cd server && cargo test -- --nocapture
+.PHONY: test-server
+
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,12 @@
 use std::env;
-use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
+use actix_web::{web,  App, HttpResponse, HttpServer, Responder};
+
+
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("gm")
 }
 
-#[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
@@ -19,10 +19,34 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
+        .route("/", web::get().to(hello))
+        .route("/echo", web::post().to(echo))
     })
     .bind(url)?
     .run()
     .await
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{test, web, App};
+
+
+    #[actix_rt::test]
+    async fn test_hello() {
+        let mut app = test::init_service(App::new().route("/", web::get().to(hello))).await;
+        let req = test::TestRequest::get().to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert_eq!(test::read_body(resp).await, "gm");
+    }
+
+    #[actix_rt::test]
+    async fn test_echo() {
+        let mut app = test::init_service(App::new().route("/echo", web::post().to(echo))).await;
+        let req = test::TestRequest::post().set_payload("gm").uri("/echo").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert_eq!(test::read_body(resp).await, "gm");
+    }
 }
